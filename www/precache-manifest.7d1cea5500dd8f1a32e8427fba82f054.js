c3a2c97b209bb6768528fe7845eb5c9cc949c5ba
Marcos Dolce <marcosdolce.91@gmail.com>
2023-07-15 21:11:14 -0300

    New react build

---
+++ www/precache-manifest.7d1cea5500dd8f1a32e8427fba82f054.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5374a8dffcb891d08f8",
+    "url": "./static/css/main.bfbcb809.chunk.css"
+  },
+  {
+    "revision": "b5374a8dffcb891d08f8",
+    "url": "./static/js/main.b5374a8d.chunk.js"
+  },
+  {
+    "revision": "79a5ffff57b35e318cdc",
+    "url": "./static/css/1.914d6f8b.chunk.css"
+  },
+  {
+    "revision": "79a5ffff57b35e318cdc",
+    "url": "./static/js/1.79a5ffff.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "./static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "c46bf12c553c43bc1f0533d208c11a25",
+    "url": "./static/media/lupa.c46bf12c.svg"
+  },
+  {
+    "revision": "bc709972b74dae19091e641dc1da351d",
+    "url": "./index.html"
+  }
+];
